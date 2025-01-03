a17facd05d451f71039cda17db4108b6fb346fca
cibi <cibikomberi@gmail.com>
2025-01-03 15:49:42 +0530

    parsed json received from mqtt

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,21 @@
 
 #include <Ethernet.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+EthernetClient ethClient;
+PubSubClient MqttClient(ethClient);
+JsonDocument json;
+
+const int warnDuration = 10;  //seconds before power off
+float durationRemaining = 0;
+unsigned long durationSetTime = 0;
+int lastStatus = 0;
+
+const int outputPin = 26;
+const int buzzerPin = 13;
+const int warningLEDPin = 15;
+const int wifiLEDPin = 32;
 
 byte mac[] = { 0x74, 0x69, 0x69, 0x2D, 0x30, 0x31 };
 IPAddress ip(10, 70, 11, 187);
@@ -9,45 +24,30 @@ IPAddress mydns(172, 16, 16, 16);
 IPAddress gateway(10, 70, 11, 1);
 IPAddress subnet(255, 255, 255, 0);
 
-String deviceId = "D001";
-String roomId = "R001";
+String deviceId = "D0001";
+String roomId = "R0001";
 
-const char* mqtt_server = "test.mosquitto.org";
+const char* mqtt_server = "10.70.11.247";
 String clientId = "clsrm-node-" + String(deviceId);
-String subTopic = "qrpower/" + roomId + "/" + deviceId;
-String pubTopic = "qrpower/" + deviceId + "/" + roomID;
+String subTopic = "qrpower/" + String(roomId) + "/" + String(deviceId);
+String pubTopic = "qrpower/" + String(deviceId) + "/" + String(roomId);
 
-EthernetClient ethClient;
-PubSubClient MqttClient(ethClient);
 
-const int warnDuration = 10;  //seconds before power off
-const int outputPin = 26;
-const int buzzerPin = 13;
-const int warningLEDPin = 15;
-const int wifiLEDPin = 32;
-float durationRemaining = 0;
-unsigned long durationSetTime = 0;
-int bookingID = 0;
-int roomID = 0;
+
 
 void parseDataFromMessage(String message) {
-  int firstComma = message.indexOf(',');
-  int lastComma = message.lastIndexOf(',');
+  DeserializationError error = deserializeJson(json, message);
 
-  if (firstComma != -1 && lastComma != -1 && firstComma != lastComma) {
-    bookingID = message.substring(0, firstComma).toInt();
-    roomID = message.substring(firstComma + 1, lastComma).toInt();
-
-    //convert seconds to float in milli seconds
-    durationRemaining = (message.substring(lastComma + 1).toFloat());
+  if (!error) {
+    durationRemaining = json["minutes"].as<float>() * 60;
     durationSetTime = millis();
 
-    Serial.println("ID: " + String(bookingID));
-    Serial.println("RoomID: " + String(roomID));
-    Serial.println("time left: " + String(durationRemaining) + "s");
-
-  } else {
-    Serial.println("Parse fail");
+    if (durationRemaining > 0){
+      JsonDocument publishDoc;
+      publishDoc["device_id"] = deviceId;
+      publishDoc["status"] = 1;
+      MqttClient.publish(pubTopic.c_str(), publishDoc.as<String>().c_str());
+    } 
   }
 }
 
@@ -62,20 +62,8 @@ void MqttCallback(char* topicIn, byte* messageIn, unsigned int length) {
     Serial.print((char)messageIn[i]);
     message += (char)messageIn[i];
   }
-  Serial.println();
 
-  // if (String(topicIn).equals(subTopic)) {
   parseDataFromMessage(message);
-  // }
-}
-
-void sendStatusUpdate() {
-  // if (WiFi.status() == WL_CONNECTED) {
-  String statusString = String(bookingID) + "," + String(roomID) + "," + String(durationRemaining - ((millis() - durationSetTime) / 1000));
-  MqttClient.publish(pubTopic.c_str(), statusString.c_str());
-  // } else {
-  //   connectWifi();
-  // }
 }
 
 void setup() {
@@ -88,18 +76,18 @@ void setup() {
   digitalWrite(buzzerPin, LOW);
   digitalWrite(warningLEDPin, LOW);
   digitalWrite(wifiLEDPin, LOW);
+
   Ethernet.init(5);
   Ethernet.begin(mac, ip, mydns, gateway, subnet);
 
   Serial.print("IP:");
   Serial.println(Ethernet.localIP());
   digitalWrite(wifiLEDPin, HIGH);
+
   MqttClient.setServer(mqtt_server, 1883);
   MqttClient.setCallback(MqttCallback);
   MqttClient.subscribe(subTopic.c_str());
 }
-
-
 
 // Ensure connection with broker
 void reconnectMqtt() {
@@ -126,12 +114,10 @@ void loop() {
     reconnectMqtt();
   }
   MqttClient.loop();
-  sendStatusUpdate();
 
-  // current time - durationSetTime gives the time after last message
   float elapsedTimeSinceLastMessage = (millis() - durationSetTime) / 1000;
-  // if last message was 10 sec ago and if duration remaining is 5 seconds it will turn off
   if (elapsedTimeSinceLastMessage < durationRemaining) {
+    lastStatus = 1;
     digitalWrite(outputPin, HIGH);
     Serial.println("Time left: " + String(durationRemaining - elapsedTimeSinceLastMessage));
   } else {
@@ -149,4 +135,14 @@ void loop() {
     digitalWrite(warningLEDPin, LOW);
   }
   delay(500);
+
+  //Send ack to server if power is transitioning to off
+  if (lastStatus == 1 && durationRemaining - elapsedTimeSinceLastMessage <= 0) {
+    JsonDocument publishDoc;
+    publishDoc["device_id"] = deviceId;
+    publishDoc["status"] = 0;
+
+    MqttClient.publish(pubTopic.c_str(), publishDoc.as<String>().c_str());
+    lastStatus = 0;
+  }  
 }
